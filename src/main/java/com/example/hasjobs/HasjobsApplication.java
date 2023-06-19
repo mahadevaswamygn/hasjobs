c85898894ab87b8758cc2e6cba7ea963db87f1ec
mahadeva swamy GN <mahadevaswamygn12345@gmail.com>
2023-06-19 11:24:13 +0530

    first commit

---
+++ src/main/java/com/example/hasjobs/HasjobsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hasjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HasjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HasjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hasjobs/HasjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hasjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
